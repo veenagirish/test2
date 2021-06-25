167882ad17dfe3308b2df408126d6fa4bbd2ecbb
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-25 17:05:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+var ground;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+
+ 
+}
+
