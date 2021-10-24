a81cdabc0eec1a91523736604a85244d7622cf43
veena c <veena.chikkanna9@gmail.com>
2021-10-24 15:04:10 +0530

    create ball

--- sketch.js
+++ sketch.js
@@ -6,16 +6,26 @@ const Body = Matter.Body;
 
 let engine;
 let world;
+var ball;
 
 function setup()
 {
   createCanvas(400,400);
+  engine=Engine.create();
+  world=engine.world;
 
 }
 
 function draw() 
 {
   background(51);
+
+  var ball_options={
+    restitution:0.95,
+    frictionAir:0.01
+  }
+  ball=Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
  
 }
 
